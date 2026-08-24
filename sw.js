7937ae1e0bb92bc8307bee1a5f8dc63dc835b1fe
chichano25 <c.montoya@icloud.com>
2026-08-23 22:27:44 -0500

    Actualiza caché para nueva configuración

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="control-clases-v3";
-const ASSETS=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE="control-clases-v4";
+const ASSETS=["./","./index.html","./styles.css","./app.js","./settings-ui.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
