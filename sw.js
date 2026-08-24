1aace518a53f9875720e4a43852fb4a70cc034cb
chichano25 <c.montoya@icloud.com>
2026-08-23 22:37:40 -0500

    Fuerza actualización de la interfaz de configuración

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="control-clases-v5";
+const CACHE="control-clases-v6";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./settings-ui.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
